0f2ccf27b4f0836a6518ada177c3f8650a282586
Stas <www.stas011@mail.ru>
2022-08-25 12:17:37 +0300

    final fix println

--- src/Main.java
+++ src/Main.java
@@ -53,10 +53,10 @@ public class Main {
                 }
 
                 if (isOnSale) {
-                    System.out.println("\t" + products[i] + " " + (doBonus ? counts[i] + 1 : counts[i]) + " шт. за " + (prices[i] * ((doBonus ? counts[i] - 1 : counts[i]) / 3 * 2 + counts[i] % 3)) + (doBonus ? " руб. (распродажа!)" : " руб. "));
+                    System.out.println("\t" + products[i] + " " + (doBonus ? counts[i] + 1 : counts[i]) + " шт. за " + (prices[i] * ((doBonus ? counts[i]  : counts[i]) / 3 * 2 + counts[i] % 3)) + (doBonus ? " руб. (распродажа!)" : " руб. "));
                     sum += prices[i] * (counts[i] / 3 * 2 + counts[i] % 3);
                 } else {
-                    System.out.println("\t" + products[i] + " " + (doBonus ? counts[i] + 1 : counts[i]) + " шт. за " + (prices[i] * counts[i]) + " руб.");
+                    System.out.println("\t" + products[i] + " " + (doBonus ? counts[i] + 1 : counts[i]) + " шт. за " + (prices[i] * counts[i]) + (doBonus ? " руб. (распродажа!)" : " руб. "));
                     sum += prices[i] * counts[i];
                 }
 
